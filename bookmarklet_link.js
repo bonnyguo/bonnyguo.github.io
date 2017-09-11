a1676a4e982e1c8e6a7462de6c58216bc903bcb1
bonnyguo <31858791+bonnyguo@users.noreply.github.com>
2017-09-11 11:02:02 -0700

    Add files via upload

---
+++ bookmarklet_link.js
@@ -0,0 +1,6 @@
+
+javascript:(function(){
+    document.body.appendChild(document.createElement('script')).src='Your Link Goes Here';
+})();
+
+// this bookmark will link to online resounce, which will constantly updated along the website
