97303a45fa108243057a764dde1348bb0f151400
bonnyguo <31858791+bonnyguo@users.noreply.github.com>
2017-09-13 08:50:15 -0700

    Add files via upload

--- bookmarklet_font.js
+++ bookmarklet_font.js
@@ -33,20 +33,15 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
     
+
+
+
 var p=document.getElementsByTagName('*');
-            for (i=0;i<p.length;i++){
-                if(p[i].style.fontSize){
-                    var s=parseInt(p[i].style.fontSize.replace('px',''));
-                }
-                else{
-                    var s=12;
-                }
-                    s+=2;
-                    p[i].style.fontSize=s+'px';
-            }
-
-
-
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
 
 
 
