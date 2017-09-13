6ac81ff10ea71670b861b0d6622d4a6d0607d29c
bonnyguo <31858791+bonnyguo@users.noreply.github.com>
2017-09-13 08:34:58 -0700

    Add files via upload

--- bookmarklet_font.js
+++ bookmarklet_font.js
@@ -36,10 +36,12 @@ function initMyBookmarklet(){
             for (i=0;i<p.length;i++){
                 if(p[i].style.fontSize){
                     var s=parseInt(p[i].style.fontSize.replace('px',''));
-                }else{
-                    var s=12;}
+                }
+                else{
+                    var s=12;
+                }
                     s+=2;
-                    p[i].style.fontSize=s+'px'
+                    p[i].style.fontSize=s+'px';
                 }
             }
 
