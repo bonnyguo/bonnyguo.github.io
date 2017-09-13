578d367e7c0099d97980bee843b236a06ae4e370
bonnyguo <31858791+bonnyguo@users.noreply.github.com>
2017-09-13 08:31:10 -0700

    Add files via upload

--- bookmarklet_font.js
+++ bookmarklet_font.js
@@ -1,5 +1,5 @@
 
-console.log('Loaded Background changing bookmarklet!!!');
+console.log('loaded font change bookmarklet');
 
 
 javascript:(function(){
@@ -32,8 +32,16 @@ function initMyBookmarklet(){
     (window.myBookmarklet = function (){
 
         //YOUR CODE GOES HERE!
-      //  javascript:void(document.body.style.font=prompt('What color would you like the background to be?'));
-       javascript: void(document.getElementById("myP").style.font-size = "3rem");
+        var p=document.getElementsByTagName('*');
+            for (i=0;i<p.length;i++){
+                if(p[i].style.fontSize){
+                    var s=parseInt(p[i].style.fontSize.replace('px',''));
+                }else{
+                    var s=12;}
+                    s+=2;
+                    p[i].style.fontSize=s+'px'
+                }
+            }
 
 
     })();
