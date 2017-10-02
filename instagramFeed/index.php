796704fee7777d6fb8199b45f46cf0ef3e588d06
bonnyguo <31858791+bonnyguo@users.noreply.github.com>
2017-10-01 19:23:09 -0700

    Add files via upload

---
+++ instagramFeed/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="css/main2.css"/>
+	<script type="text/javascript" src="http://code.jquery-latest.js"></script>
+	<script src="js/script2.js"></script>
+	<title> Instagram Feed @ NYSomeTimes </title>
+</head>
+
+<body>
+
+<div id='instafeed'></div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='./js/instafeed.min.js'></script>
+<script src='./js/custom.js'></script>
+
+
+</body>
+</html>
